a76fe16eaef7648d706b9e1480d35dd8de1a8821
pftom <1043269994@qq.com>
2021-01-26 18:51:57 +0800

    fix: 修复 React 热更新

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,15 +6,14 @@ import styled from "@emotion/styled";
 
 const ListItem = styled.li`
   color: palevioletred;
-  margin: 0.5em 1em;
-  padding: 0.25em 1em;
+  margin: 0.5em 2em;
+  padding: 0.25em 2em;
   background: transparent;
 `;
 
 function Home() {
   return (
     <ol className="home">
-      <ListItem>你好</ListItem>
       <ListItem>再见</ListItem>
       <ListItem>我来了</ListItem>
       <ListItem>猪笨笨</ListItem>
